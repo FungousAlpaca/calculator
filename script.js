312a5fbb505b28af0d50c47b5fe379f4d81a4561
Drake Williamson <williamson.drake@outlook.com>
2023-01-16 23:33:45 -0800

    Change display values and idisplay texts to strings

--- script.js
+++ script.js
@@ -36,143 +36,143 @@ const equals = document.querySelector('#equals');
 const clear = document.querySelector('#clear');
 
 const bottomDisplay = document.querySelector('.display-text-bottom');
-let displayValue = 0;
+let displayValue = '0';
 
 
 zero.addEventListener('click', () => {
 
-   if (displayValue === 0) {
-        bottomDisplay.textContent = 0;
-        displayValue += 0;
+   if (displayValue === '0') {
+        bottomDisplay.textContent = '0';
+        displayValue = '0';
         console.log(`Display value = ${displayValue}`);
     } else {
-        bottomDisplay.textContent += 0;
-        displayValue += 0;
+        bottomDisplay.textContent += '0';
+        displayValue += '0';
         console.log(`Display value = ${displayValue}`);
 
     }
 });
 
 one.addEventListener('click', () => {
-    if (displayValue === 0) {
-        bottomDisplay.textContent = 1;
-        displayValue += 1;
+    if (displayValue === '0') {
+        bottomDisplay.textContent = '1';
+        displayValue = '1';
         console.log(`Display value = ${displayValue}`);
     } else {
-        bottomDisplay.textContent += 1;
-        displayValue += 1;
+        bottomDisplay.textContent += '1';
+        displayValue += '1';
         console.log(`Display value = ${displayValue}`);
 
     }
 });
 
 two.addEventListener('click', () => {
-    if (displayValue === 0) {
-        bottomDisplay.textContent = 2;
-        displayValue += 2;
+    if (displayValue === '0') {
+        bottomDisplay.textContent = '2';
+        displayValue = '2';
         console.log(`Display value = ${displayValue}`);
     } else {
-        bottomDisplay.textContent += 2;
-        displayValue += 2;
+        bottomDisplay.textContent += '2';
+        displayValue += '2';
         console.log(`Display value = ${displayValue}`);
 
     }
 });
 
 three.addEventListener('click', () => {
-    if (displayValue === 0) {
-        bottomDisplay.textContent = 3;
-        displayValue += 3;
+    if (displayValue === '0') {
+        bottomDisplay.textContent = '3';
+        displayValue = '3';
         console.log(`Display value = ${displayValue}`);
     } else {
-        bottomDisplay.textContent += 3;
-        displayValue += 3;
+        bottomDisplay.textContent += '3';
+        displayValue += '3';
         console.log(`Display value = ${displayValue}`);
 
     }
 });
 
 four.addEventListener('click', () => {
-    if (displayValue === 0) {
-        bottomDisplay.textContent = 4;
-        displayValue += 4;
+    if (displayValue === '0') {
+        bottomDisplay.textContent = '4';
+        displayValue = '4';
         console.log(`Display value = ${displayValue}`);
     } else {
-        bottomDisplay.textContent += 4;
-        displayValue += 4;
+        bottomDisplay.textContent += '4';
+        displayValue += '4';
         console.log(`Display value = ${displayValue}`);
 
     }
 });
 
 five.addEventListener('click', () => {
-    if (displayValue === 0) {
-        bottomDisplay.textContent = 5;
-        displayValue += 5;
+    if (displayValue === '0') {
+        bottomDisplay.textContent = '5';
+        displayValue = '5';
         console.log(`Display value = ${displayValue}`);
     } else {
-        bottomDisplay.textContent += 5;
-        displayValue += 5;
+        bottomDisplay.textContent += '5';
+        displayValue += '5';
         console.log(`Display value = ${displayValue}`);
 
     }
 });
 
 six.addEventListener('click', () => {
-    if (displayValue === 0) {
-        bottomDisplay.textContent = 6;
-        displayValue += 6;
+    if (displayValue === '0') {
+        bottomDisplay.textContent = '6';
+        displayValue = '6';
         console.log(`Display value = ${displayValue}`);
     } else {
-        bottomDisplay.textContent += 6;
-        displayValue += 6;
+        bottomDisplay.textContent += '6';
+        displayValue += '6';
         console.log(`Display value = ${displayValue}`);
 
     }
 });
 
 seven.addEventListener('click', () => {
-    if (displayValue === 0) {
-        bottomDisplay.textContent = 7;
-        displayValue += 7;
+    if (displayValue === '0') {
+        bottomDisplay.textContent = '7';
+        displayValue = '7';
         console.log(`Display value = ${displayValue}`);
     } else {
-        bottomDisplay.textContent += 7;
-        displayValue += 7;
+        bottomDisplay.textContent += '7';
+        displayValue += '7';
         console.log(`Display value = ${displayValue}`);
 
     }
 });
 
 eight.addEventListener('click', () => {
-    if (displayValue === 0) {
-        bottomDisplay.textContent = 8;
-        displayValue += 8;
+    if (displayValue === '0') {
+        bottomDisplay.textContent = '8';
+        displayValue = '8';
         console.log(`Display value = ${displayValue}`);
     } else {
-        bottomDisplay.textContent += 8;
-        displayValue += 8;
+        bottomDisplay.textContent += '8';
+        displayValue += '8';
         console.log(`Display value = ${displayValue}`);
 
     }
 });
 
 nine.addEventListener('click', () => {
-    if (displayValue === 0) {
-        bottomDisplay.textContent = 9;
-        displayValue += 9;
+    if (displayValue === '0') {
+        bottomDisplay.textContent = '9';
+        displayValue = '9';
         console.log(`Display value = ${displayValue}`);
     } else {
-        bottomDisplay.textContent += 9;
-        displayValue += 9;
+        bottomDisplay.textContent += '9';
+        displayValue += '9';
         console.log(`Display value = ${displayValue}`);
 
     }
 });
 
 clear.addEventListener('click', () => {
-    bottomDisplay.textContent = 0;
-    displayValue = 0;
+    bottomDisplay.textContent = '0';
+    displayValue = '0';
     console.log(`Display value = ${displayValue}`);
 })
 
