2d3124efc0168b900c752a73423fc5bf0a8e6735
Drake Williamson <williamson.drake@outlook.com>
2023-01-17 21:51:01 -0800

    Remove repetition for operators

--- script.js
+++ script.js
@@ -1,164 +1,103 @@
 function add(num1, num2) {
-    return num1 + num2;
-};
+  return num1 + num2;
+}
 
 function subtract(num1, num2) {
-    return num1 - num2;
-};
+  return num1 - num2;
+}
 
 function multiply(num1, num2) {
-    return num1 * num2;
-};
+  return num1 * num2;
+}
 
 function divide(num1, num2) {
-    return num1 / num2;
-};
+  return num1 / num2;
+}
 
 function operate(operator, num1, num2) {
-    return operator(num1, num2);
-};
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "x") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return divide(num1, num2);
+  }
+}
 
-const division = document.querySelector('#divide');
-const multiplication = document.querySelector('#multiply');
-const minus = document.querySelector('#minus');
-const plus = document.querySelector('#plus');
-const equals = document.querySelector('#equals');
-const clear = document.querySelector('#clear');
-const bottomDisplay = document.querySelector('.display-text-bottom');
-const digits = document.querySelectorAll('.digit');
-let displayValue = '0';
+const division = document.querySelector("#divide");
+const multiplication = document.querySelector("#multiply");
+const minus = document.querySelector("#minus");
+const plus = document.querySelector("#plus");
+const equals = document.querySelector("#equals");
+const clear = document.querySelector("#clear");
+const bottomDisplay = document.querySelector(".display-text-bottom");
+const digits = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".op");
+let displayValue = "0";
 let previousTotal = 0;
 let operation = undefined;
 let operandOne = undefined;
 let operandTwo = undefined;
 
-digits.forEach(digit => {
-    digit.addEventListener('click', () => {
-
-   if (displayValue === '0') {
-        bottomDisplay.textContent = `${digit.getAttribute('id')}`;
-        displayValue = `${digit.getAttribute('id')}`;
-        console.log(`Display value = ${displayValue}`);
+digits.forEach((digit) => {
+  digit.addEventListener("click", () => {
+    if (displayValue === "0") {
+      bottomDisplay.textContent = `${digit.getAttribute("id")}`;
+      displayValue = `${digit.getAttribute("id")}`;
     } else if (displayValue.length < 9) {
-        bottomDisplay.textContent += `${digit.getAttribute('id')}`;
-        displayValue += `${digit.getAttribute('id')}`;
-        console.log(`Display value = ${displayValue}`);
-
+      bottomDisplay.textContent += `${digit.getAttribute("id")}`;
+      displayValue += `${digit.getAttribute("id")}`;
     }
   });
 });
 
-clear.addEventListener('click', () => {
-    bottomDisplay.textContent = '0';
-    displayValue = '0';
-    console.log(`Display value = ${displayValue}`);
-    operation = undefined;
-    operandOne = undefined;
-    operandTwo = undefined;
-})
-
-plus.addEventListener('click', () => {
+operators.forEach((op) => {
+  op.addEventListener("click", () => {
     if (operation !== undefined && operandOne !== undefined) {
-    operandTwo = Number(displayValue);
-    if (operandTwo === 0) {
-        alert('Did you really just try that?')
-        operation = undefined;
-        operandOne = undefined;
-        operandTwo = undefined;
+      operandTwo = Number(displayValue);
+      let previousTotal = Math.round(
+        operate(operation, operandOne, operandTwo)
+      );
+      bottomDisplay.textContent = previousTotal;
+      operation = `${op.getAttribute("id")}`;
+      operandOne = previousTotal;
+      displayValue = "0";
     } else {
-    let previousTotal = Math.round(operate(operation, operandOne, operandTwo));
-    bottomDisplay.textContent = previousTotal;
-    operation = add;
-    operandOne = previousTotal
-    displayValue = '0';
+      operandOne = Number(displayValue);
+      operation = `${op.getAttribute("id")}`;
+      displayValue = "0";
     }
-    } else {
-    operandOne = Number(displayValue);
-    operation = add;
-    displayValue = '0';
-  }
+  });
 });
 
-minus.addEventListener('click', () => {
-    if (operation !== undefined && operandOne !== undefined) {
-    operandTwo = Number(displayValue);
-    if (operandTwo === 0) {
-        alert('Did you really just try that?')
-        operation = undefined;
-        operandOne = undefined;
-        operandTwo = undefined;
-    } else {
-    let previousTotal = Math.round(operate(operation, operandOne, operandTwo));
-    bottomDisplay.textContent = previousTotal;
-    operation = subtract;
-    operandOne = previousTotal
-    displayValue = '0';
-    }
-    } else {
-    operandOne = Number(displayValue);
-    operation = subtract;
-    displayValue = '0';
-  }
+clear.addEventListener("click", () => {
+  bottomDisplay.textContent = "0";
+  displayValue = "0";
+  console.log(`Display value = ${displayValue}`);
+  operation = undefined;
+  operandOne = undefined;
+  operandTwo = undefined;
 });
 
-multiplication.addEventListener('click', () => {
-    if (operation !== undefined && operandOne !== undefined) {
-    operandTwo = Number(displayValue);
-    if (operandTwo === 0) {
-        alert('Did you really just try that?')
-        operation = undefined;
-        operandOne = undefined;
-        operandTwo = undefined;
-    } else {
-    let previousTotal = Math.round(operate(operation, operandOne, operandTwo));
-    bottomDisplay.textContent = previousTotal;
-    operation = multiply;
-    operandOne = previousTotal
-    displayValue = '0';
-    }
-    } else {
-    operandOne = Number(displayValue);
-    operation = multiply;
-    displayValue = '0';
-  }
-});
-
-division.addEventListener('click', () => {
-    if (operation !== undefined && operandOne !== undefined) {
-    operandTwo = Number(displayValue);
-    if (operandTwo === 0) {
-        alert('Did you really just try that?')
-        operation = undefined;
-        operandOne = undefined;
-        operandTwo = undefined;
-    } else {
-    let previousTotal = Math.round(operate(operation, operandOne, operandTwo));
-    bottomDisplay.textContent = previousTotal;
-    operation = divide;
-    operandOne = previousTotal
-    displayValue = '0';
-    }
-    } else {
-    operandOne = Number(displayValue);
-    operation = divide;
-    displayValue = '0';
-  }
-});
-
-equals.addEventListener('click', () => {
-
-    if (operation !== undefined || operandOne !== undefined || operandTwo !== undefined) {
-
+equals.addEventListener("click", () => {
+  if (
+    operation !== undefined ||
+    operandOne !== undefined ||
+    operandTwo !== undefined
+  ) {
     operandTwo = Number(displayValue);
 
     if (operandTwo === 0 && operation === divide) {
-        alert('Did you really just try that?')
-        operation = undefined;
-        operandOne = undefined;
-        operandTwo = undefined;
+      alert("Did you really just try that?");
+      operation = undefined;
+      operandOne = undefined;
+      operandTwo = undefined;
     } else {
-    bottomDisplay.textContent = Math.round(operate(operation, operandOne, operandTwo));
+      bottomDisplay.textContent = Math.round(
+        operate(operation, operandOne, operandTwo)
+      );
     }
   }
 });
-
